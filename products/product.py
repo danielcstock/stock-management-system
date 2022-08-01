93ec478cf69c120079908649f9d3bc878fd8ebb5
rafaelcstock@gmail.com <rafaelcstock@gmail.com>
2022-07-31 21:57:47 -0300

    Preço

--- products/product.py
+++ products/product.py
@@ -1,4 +1,4 @@
 name = "Notebook HP Intel Core i3"
-preço = "apenas 9.999,60  ta barato pra car****"
+preço = "Apenas 9.999,60  ta barato pra car****"
 print("Nome: " + name)
 print("Preço: " + preço)
