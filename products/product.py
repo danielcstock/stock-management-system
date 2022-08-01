81356ba81180504cfa5e688bda45574429b6181c
rafaelcstock@gmail.com <rafaelcstock@gmail.com>
2022-07-31 22:14:00 -0300

    Ok

--- products/product.py
+++ products/product.py
@@ -1,4 +1,12 @@
-name = "Notebook HP Intel Core i3"
-preço = "Apenas 9.999,60  ta barato pra car****"
+name = "Notebook HP Intel Core i3 (Windows 98)"
+preço = "Apenas $9.999,60 á vista ou 12x de 2.000"
+Estoque = "10 unidades"
+Desconto = "50% "
+Frete = "Frete grátis para o sul do Brasil"
+
+
 print("Nome: " + name)
 print("Preço: " + preço)
+print("Disponível:" + Estoque)
+print("Desconto de " + Desconto + " no Pix ou Boleto")
+print("Envio Rápido e " + Frete )
